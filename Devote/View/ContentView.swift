7d202153fa67d573df356d848eddffb97db1edf4
sadikcoban <sadik.coban@agu.edu.tr>
2021-09-10 10:45:37 +0300

    custom visual effect is added to main page, along with a blurance to new task item page

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -88,9 +88,15 @@ struct ContentView: View {
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
                 }
+                .blur(radius: showNewTaskItem ? 8.0 : 0,opaque: false )
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
+                
                 //MARK: - New Task Item
                 if showNewTaskItem {
-                    BlankView()
+                    BlankView(
+                        backgroundColor: isDarkMode ? .black  : .gray,
+                        backgroundOpacity: isDarkMode ? 0.3 : 0.5)
                         .onTapGesture {
                             withAnimation {
                                 showNewTaskItem = false
@@ -104,6 +110,7 @@ struct ContentView: View {
             }
             .navigationBarHidden(true)
             .background(BackgroundImageView())
+            .blur(radius: showNewTaskItem ? 8 : 0,  opaque: false)
             .background(backgroundGradient.ignoresSafeArea(.all))
         }//:Navigation
         .navigationViewStyle(StackNavigationViewStyle())
